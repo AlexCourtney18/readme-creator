7e3ae7464f19492c8cd230cbcd71c442d5c3747b
Alex <lex.courtney18@gmail.com>
2022-07-20 15:27:30 -0500

    install inquirer, update gitignore, and add first question

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,24 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the project title!');
+                    return false;
+                }
+            }
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +27,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+questions();
